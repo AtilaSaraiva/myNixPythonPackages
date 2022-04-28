9da1c7f70ac35304007222c20dbaea4bdf0fac11
Átila Saraiva <atilasaraiva@gmail.com>
2022-04-28 12:11:36 -0300

    changed the folder structure

--- default.nix
+++ default.nix
@@ -1,22 +1,22 @@
 self: super:
 rec {
-  codepy = super.python3Packages.callPackage ./pkgs/codepy {
+  codepy = super.python3Packages.callPackage ./pkgs/python-packages/codepy {
     inherit (self.python3Packages) pytools appdirs six cgen;
     inherit (self) fetchFromGitHub;
   };
 
-  contexttimer = super.python3Packages.callPackage ./pkgs/contexttimer {
+  contexttimer = super.python3Packages.callPackage ./pkgs/python-packages/contexttimer {
     inherit (self.python3Packages) mock;
     inherit (self) fetchFromGitHub fetchpatch;
   };
 
-  pyrevolve = super.python3Packages.callPackage ./pkgs/pyrevolve {
+  pyrevolve = super.python3Packages.callPackage ./pkgs/python-packages/pyrevolve {
     inherit (self.python3Packages) versioneer cython numpy pytest;
     inherit (self) fetchFromGitHub;
     contexttimer = self.contexttimer;
   };
 
-  devito = super.python3Packages.callPackage ./pkgs/devito {
+  devito = super.python3Packages.callPackage ./pkgs/python-packages/devito {
     inherit (self.python3Packages) anytree nbval sympy scipy cached-property psutil py-cpuinfo cgen click multidict distributed pytestCheckHook matplotlib pytest-xdist;
     inherit (self) fetchFromGitHub;
     pyrevolve = self.pyrevolve;
