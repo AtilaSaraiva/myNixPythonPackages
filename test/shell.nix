0f3e83dda968382632981fb32be5fec076012bd4
Átila Saraiva <atilasaraiva@gmail.com>
2022-04-28 11:24:37 -0300

    added test nix-shell

---
+++ test/shell.nix
@@ -0,0 +1,15 @@
+# python.nix
+with (import <nixpkgs> { overlays = [ (import ../.) ];});
+let
+  my-python-packages = python-packages: with python-packages; [
+    codepy
+    # other python packages you want
+  ];
+  python-with-my-packages = python3.withPackages my-python-packages;
+in
+mkShell {
+  buildInputs = [
+    python-with-my-packages
+  ];
+}
+
